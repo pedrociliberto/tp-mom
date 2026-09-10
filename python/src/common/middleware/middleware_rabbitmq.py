482af6abdc36bafc94ae7915244e51e85ffd467b
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-09-10 17:26:00 -0300

    Agrego metodos con pass

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -8,7 +8,31 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     def __init__(self, host, queue_name):
         pass
 
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
+        pass
+
+    def send(self, message):
+        pass
+
+    def close(self):
+        pass
+    
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
+        pass
+
+    def send(self, message):
+        pass
+
+    def close(self):
         pass
