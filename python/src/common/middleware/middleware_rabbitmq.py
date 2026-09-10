7021f59f3ed0f9323fd8d7dc8570460175a113e6
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-09-10 18:24:32 -0300

    Primera implementacion Exchange

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -37,16 +37,36 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+        self.routing_keys = routing_keys
+        self.exchange_name = exchange_name
 
     def start_consuming(self, on_message_callback):
-        pass
+        def _internal_callback(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            def nack():
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+            on_message_callback(body, ack, nack)
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        queue_name = result.method.queue
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name, queue=queue_name, routing_key=routing_key)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=_internal_callback, auto_ack=False)
+        self.channel.start_consuming()
 
     def stop_consuming(self):
-        pass
+        self.channel.stop_consuming()
 
     def send(self, message):
-        pass
+        for routing_key in self.routing_keys:
+            self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key, body=message)
 
     def close(self):
-        pass
+        if self.channel.is_open:
+            self.channel.close()
+        if self.connection.is_open:
+            self.connection.close()
