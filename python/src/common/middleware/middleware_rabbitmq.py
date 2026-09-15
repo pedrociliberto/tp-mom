eaf044b7105ad412b4dbfcc0d639561de9232e27
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-09-15 16:24:57 -0300

    refactor: new class Base for common methods and behaviour

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -17,30 +17,31 @@ DISCONNECTION_ERRORS = (
     pika.exceptions.StreamLostError,
 )
 DISCONNECTION_MSG = "Error connecting to RabbitMQ: {}"
-class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
-    def __init__(self, host, queue_name):
+class _MessageMiddlewareRabbitMQBase:
+    def __init__(self, host):
         try:
             self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
             self.channel = self.connection.channel()
-            self.channel.queue_declare(queue=queue_name, durable=True)
-            self.queue_name = queue_name
             self.is_consuming = False
         except DISCONNECTION_ERRORS as e:
             raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
         except Exception as e:
             raise MessageMiddlewareMessageError(e) from e
 
-    def start_consuming(self, on_message_callback):
-        try: 
-            def _internal_callback(ch, method, properties, body):
-                def ack():
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                def nack():
-                    ch.basic_nack(delivery_tag=method.delivery_tag)
-                on_message_callback(body, ack, nack)
+    def _wrap_callback(self, on_message_callback):
+        def _internal_callback(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            def nack():
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+            on_message_callback(body, ack, nack)
+        return _internal_callback
 
-            self.channel.basic_consume(queue=self.queue_name, on_message_callback=_internal_callback, auto_ack=False)
+    def _consume(self, queue_name, on_message_callback):
+        try:
+            callback = self._wrap_callback(on_message_callback)
+            self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
             self.is_consuming = True
             self.channel.start_consuming()
         except DISCONNECTION_ERRORS as e:
@@ -55,14 +56,6 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             if self.is_consuming:
                 self.channel.stop_consuming()
                 self.is_consuming = False
-        except DISCONNECTION_ERRORS as e:
-            raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
-        except Exception as e:
-            raise MessageMiddlewareMessageError(e) from e
-
-    def send(self, message):
-        try:
-            self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
         except DISCONNECTION_ERRORS as e:
             raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
         except Exception as e:
@@ -76,17 +69,38 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
                 self.connection.close()
         except Exception as e:
             raise MessageMiddlewareCloseError(e) from e
+        
+class MessageMiddlewareQueueRabbitMQ(_MessageMiddlewareRabbitMQBase, MessageMiddlewareQueue):
+
+    def __init__(self, host, queue_name):
+        super().__init__(host)
+        try:
+            self.channel.queue_declare(queue=queue_name, durable=True)
+            self.queue_name = queue_name
+        except DISCONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e) from e
+
+    def start_consuming(self, on_message_callback):
+        self._consume(self.queue_name, on_message_callback)
+
+    def send(self, message):
+        try:
+            self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
+        except DISCONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e) from e
     
-class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
+class MessageMiddlewareExchangeRabbitMQ(_MessageMiddlewareRabbitMQBase, MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
+        super().__init__(host)
         try:
-            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-            self.channel = self.connection.channel()
             self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
-            self.routing_keys = routing_keys
             self.exchange_name = exchange_name
-            self.is_consuming = False
+            self.routing_keys = routing_keys
         except DISCONNECTION_ERRORS as e:
             raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
         except Exception as e:
@@ -94,32 +108,11 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
 
     def start_consuming(self, on_message_callback):
         try:
-            def _internal_callback(ch, method, properties, body):
-                def ack():
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                def nack():
-                    ch.basic_nack(delivery_tag=method.delivery_tag)
-                on_message_callback(body, ack, nack)
-
             result = self.channel.queue_declare(queue='', exclusive=True)
             queue_name = result.method.queue
             for routing_key in self.routing_keys:
                 self.channel.queue_bind(exchange=self.exchange_name, queue=queue_name, routing_key=routing_key)
-            self.channel.basic_consume(queue=queue_name, on_message_callback=_internal_callback, auto_ack=False)
-            self.is_consuming = True
-            self.channel.start_consuming()
-        except DISCONNECTION_ERRORS as e:
-            raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
-        except Exception as e:
-            raise MessageMiddlewareMessageError(e) from e
-        finally:
-            self.is_consuming = False
-
-    def stop_consuming(self):
-        try:
-            if self.is_consuming:
-                self.channel.stop_consuming()
-                self.is_consuming = False
+            self._consume(queue_name, on_message_callback)
         except DISCONNECTION_ERRORS as e:
             raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
         except Exception as e:
@@ -133,12 +126,3 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
             raise MessageMiddlewareDisconnectedError(DISCONNECTION_MSG.format(str(e))) from e
         except Exception as e:
             raise MessageMiddlewareMessageError(e) from e
-
-    def close(self):
-        try:
-            if hasattr(self, 'channel') and self.channel and self.channel.is_open:
-                self.channel.close()
-            if hasattr(self, 'connection') and self.connection and self.connection.is_open:
-                self.connection.close()
-        except Exception as e:
-            raise MessageMiddlewareCloseError(e) from e
